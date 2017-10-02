08dde36cbf35a0ffcd6a2eb7c60d6028a8c64e2c
Sebastian Sproesser <elzoido@elbloggo.de>
2017-10-02 11:34:03 +0200

    Kommentare geputzt, Debugging eingebaut, sprechendere Variablennamen

--- Tic-Tac-Toe.ino
+++ Tic-Tac-Toe.ino
@@ -1,29 +1,35 @@
-int Spielfeld[9];
-int Spieler;
-int Gewinner;
+#define BUTTON_PIN A7
+#define DEBUG 0
+
+// Die 9 Felder des Spielfelds.
+// Werte: 0 => Noch niemand das Feld belegt
+//        1 => Spieler 1 hat das Feld belegt
+//        2 => Spieler 2 hat das Feld belegt
+short Spielfeld[9];
+short AktiverSpieler = 0;
 
 // Alle Pins, die mit den LEDs verbunden sind, auf Ausgang setzen
-void Init( void )
+void InitIO( void )
 {
-  for (int i = 2; i < 15; i++)
+  for (int pin = 2; pin < 15; pin++)
   {
-    pinMode(i, OUTPUT);
+    pinMode(pin, OUTPUT);
   }
 }
 
-// Um alle LEDs aus zu schalten, müssen die damit verbundenen Pins auf LOW gesetzt werden.
+// Um alle LEDs auszuschalten, müssen die damit verbundenen Pins auf LOW gesetzt werden.
 void AllesAus( void )
 {
-  for (int i = 2; i < 15; i++)
+  for (int pin = 2; pin < 15; pin++)
   {
-    // Der Pin 11 muss abeer auf HIGH gesetzt werden, weil er nicht mit einem Transistor verbunden ist
-    if( i != 11)
+    // Der Pin 11 muss aber auf HIGH gesetzt werden, weil er nicht mit einem Transistor verbunden ist
+    if( pin != 11)
     {
-      digitalWrite(i, HIGH);
+      digitalWrite(pin, HIGH);
     }
     else
     {
-      digitalWrite(i, LOW);
+      digitalWrite(pin, LOW);
     }
   }
 }
@@ -37,24 +43,25 @@ void InitVariablen( void )
     Spielfeld[i] = 0;
   }
   
-  // Spieler 1 darf mit dem Spiel beginnen
-  Spieler = 1;
+  // Wenn nichts anderweitig festgesetzt wurde, beginnt Spieler 1
+  if (!AktiverSpieler)
+  {
+    AktiverSpieler = 1;
+  }
 
-  // Noch hat niemand gewonnen. Deshalb wird diese Variable mit 0 belegt
-  Gewinner = 0;
 }
 
 // Diese Funktion liefert zurück, welche Taste atuell gedrückt wird.
 // Alle Tasten bilden einen Spannungsteiler, mit dem das Verhältnis der Widerstände zueinander über einen AD-Wandler ausgelesen wird.
-// Falls keine Tate gedrückt wird, zieht der Pulldown-Widerstand die Spannung auf 0V
-int TasteGedrueckt( void )// ------------------------------------ ADC neu einlesen -----------------------------------------
+// Falls keine Taste gedrückt wird, zieht der Pulldown-Widerstand die Spannung auf 0V
+int TasteGedrueckt( void )
 {
   // 10 bedeutet, dass keine Taste gedrückt wird
   int Taste = 10;
 
-  int ADC_Wert = analogRead( A7 );
+  int ADC_Wert = analogRead( BUTTON_PIN );
 
-  // Im Folgenen werden die Werte des AD-Wandlers verglichen. daraus wird dann ermittelt, welche taste gedrückt wird
+  // Im Folgenden werden die Werte des AD-Wandlers verglichen. Daraus wird dann ermittelt, welche Taste gedrückt ist.
   
   if ( (ADC_Wert > 850) && (ADC_Wert < 860) )
   {
@@ -92,10 +99,15 @@ int TasteGedrueckt( void )// ------------------------------------ ADC neu einles
   {
     Taste = 8;
   }
+  
+  if (DEBUG && Taste != 10)
+  {
+    Serial.print("Gemessener Wert: ");
+    Serial.print(ADC_Wert);
+    Serial.print(" - Gedrückte Taste: ");
+    Serial.println( Taste );
+  }
 
-  //Serial.println( Taste );
-
-  // Der ermittelte wert für die Taste wird anschließend zurückgegeben
   return Taste;
 }
 
@@ -103,7 +115,7 @@ int TasteGedrueckt( void )// ------------------------------------ ADC neu einles
 void AusgabeStatusLED( void )
 {
   // Je nachdem, welcher Spieler gerade dran ist, leuchtet die LED in einer bestimmten Farbe
-  switch (Spieler)
+  switch (AktiverSpieler)
   {
     case 0: // Allle Farben aus
         digitalWrite(5, HIGH);
@@ -143,10 +155,10 @@ void AusgabeStatusLED( void )
 void AusgabeLEDs()
 {
   // Das wird für alle 3 Zeilen widerholt
-  for(int i = 0; i < 3; i++)
+  for(int zeile = 0; zeile < 3; zeile++)
   {
     // Prüfe die LEDs 0, 3 und 6
-    switch ( Spielfeld[ 3*i ] )
+    switch ( Spielfeld[ 3*zeile ] )
     {
       case 0: // alle Farben aus
         digitalWrite(2, HIGH);
@@ -171,7 +183,7 @@ void AusgabeLEDs()
     }
 
     // Pruefe die LEDs 1, 4 und 7
-    switch ( Spielfeld[ 3*i + 1 ] )
+    switch ( Spielfeld[ 3*zeile + 1 ] )
     {
       case 0: // alle Farben aus
         digitalWrite(5, HIGH);
@@ -196,7 +208,7 @@ void AusgabeLEDs()
     }
 
     // Pruefe die LEDs 2, 5 und 8
-    switch ( Spielfeld[ 3*i + 2 ] )
+    switch ( Spielfeld[ 3*zeile + 2 ] )
     {
       case 0: // alle Farben aus
         digitalWrite(11, HIGH);
@@ -222,7 +234,7 @@ void AusgabeLEDs()
 
     // Schalte die dazugehörigr gemeinsame Kathode an.
     // Die Zustände an den Kathoden werden durch den Transistor invertiert
-    switch(i)
+    switch(zeile)
     {
       case 0: digitalWrite(6, LOW); break;
       case 1: digitalWrite(9, LOW); break;
@@ -238,96 +250,102 @@ void AusgabeLEDs()
 }
 
 // Nach jedem Spielzug muss geprüft werden ob es einen Gewinner gibt
-// Dazu werden einfach die Werte in dem Array Spielfeld zeilenweise uns spaltenweise multipliziert
-void PruefeGewinner( void )
+// Dazu werden die Werte in dem Array zeilenweise, spaltenweise und diagonal multipliziert
+// Ein Produkt ist 1, genau dann wenn alle drei Faktoren 1 sind, alle Felder von Spieler 1 belegt wurden und dieser gewonnen hat.
+// Ein Produkt ist 8, genau dann wenn alle drei Faktoren 2 sind, alle Felder von Spieler 2 belegt wurden und dieser gewonnen hat.
+
+// Rückgabewerte:
+// -1 => Unentschieden
+// 0  => Spiel läuft noch, niemand hat gewonnen
+// 1  => Spieler 1 hat gewonnen
+// 2  => Spieler 2 hat gewonnen
+
+int PruefeGewinner( void )
 {
   int Produkt = 1;
 
-  // Pruefe zuerst, ob es ein Unentschieden gibt
-  for(int i = 0; i < 9; i++)
-  {
-    Produkt *= Spielfeld[i];
-  }
-  if(Produkt != 0)
-  {
-    Gewinner = -1;
-  }
-
-  // Pruefe alle 3 Zeilen und Spalten
+  // Prüfe alle 3 Zeilen und Spalten
   for(int i = 0; i < 3; i++)
   {
-    Produkt = Spielfeld[0+i]*Spielfeld[3+i]*Spielfeld[6+i];
+    int Spalte = i;
+    Produkt = Spielfeld[ 0 + Spalte ] * Spielfeld[ 3 + Spalte ] * Spielfeld[ 6 + Spalte ];
     if(Produkt == 1)
     {
-      Gewinner = 1;
+      return 1;
     }
     if(Produkt == 8)
     {
-      Gewinner = 2;
+      return 2;
     }
-
-    Produkt = Spielfeld[0+i*3]*Spielfeld[1+3*i]*Spielfeld[2+3*i];
+    
+    int Zeile = i;
+    Produkt = Spielfeld [ 0 + Zeile * 3 ] * Spielfeld[ 1 + Zeile * 3 ] * Spielfeld[ 2 + Zeile * 3 ];
     if(Produkt == 1)
     {
-      Gewinner = 1;
+      return 1;
     }
     if(Produkt == 8)
     {
-      Gewinner = 2;
+      return 2;
     }
   }
 
-  // Pruefe die 2 Diagonalen
+  // Prüfe die 2 Diagonalen
   Produkt = Spielfeld[0] * Spielfeld[4] * Spielfeld[8];
   if(Produkt == 1)
   {
-    Gewinner = 1;
+    return 1;
   }
   if(Produkt == 8)
   {
-    Gewinner = 2;
+    return 2;
   }
 
   Produkt = Spielfeld[2] * Spielfeld[4] * Spielfeld[6];
   if(Produkt == 1)
   {
-    Gewinner = 1;
+    return 1;
   }
   if(Produkt == 8)
   {
-    Gewinner = 2;
+    return 2;
   }
+
+  // Prüfe, ob alle Felder belegt sind. Dies ist genau dann der Fall, wenn keins der Felder 0 ist
+  // und somit das Produkt aller Felder nicht 0 ist
+  for(int feld = 0; feld < 9; feld++)
+  {
+    Produkt *= Spielfeld[feld];
+  }
+  if(Produkt != 0)
+  {
+    return -1;
+  }
+
+  return 0;
 }
 
 // Falls das Spiel gewonnen wurde, zeige eine dazugehörige Animation
-void SpielGewonnen( void )
+void SpielGewonnen( int Gewinner )
 {
   bool neuesSpiel = false;
 
-  // Zuerst bleibt die Anzeig für 2 Sekunden auf dem aktuellen Spielstand
+  // Zuerst bleibt die Anzeige für 2 Sekunden auf dem aktuellen Spielstand
   unsigned long Counter = millis();
   while((millis() - 2000) < Counter)
   {
     AusgabeLEDs();
   }
 
-  // Danach wird noch zwischengespeichert, welcher Spieler als nächstes an der Reihe wäre
-  // Das ist notwendig, weil später dieser Wert verloren geht
-  int naechsterSpieler = Gewinner + 1;
-  if (naechsterSpieler > 2)
-  {
-    naechsterSpieler = 1;
-  }
-
   // Solange keine Taste gedrückt wird, wird nun diese Animation angezeigt
   do
   {
     // Teil 1
-    for(int i = 0; i < 9; i++)
+    for(int feld = 0; feld < 9; feld++)
     {
-      Spielfeld[i] = Gewinner;
+      Spielfeld[feld] = Gewinner;
     }
-    Spielfeld[4] = 0;
+    Spielfeld[4] = 0; // Mittlere LED
 
     Counter = millis();
     while((millis() - 1000) < Counter)
@@ -341,11 +359,11 @@ void SpielGewonnen( void )
     }
 
     // Teil 2
-    for(int i = 0; i < 9; i++)
+    for(int feld = 0; feld < 9; feld++)
     {
-      Spielfeld[i] = 0;
+      Spielfeld[feld] = 0;
     }
-    Spielfeld[4] = Gewinner;
+    Spielfeld[4] = Gewinner; // Mittlere LED
 
     Counter = millis();
     while((millis() - 1000) < Counter)
@@ -359,29 +377,20 @@ void SpielGewonnen( void )
     }
   }while(!neuesSpiel);
 
-  // eine Taste wurde gedrückt. Das Spiel kann weitergehen
+  // Eine Taste wurde gedrückt, ein neues Spiel beginnt
   InitVariablen();
-  Spieler = naechsterSpieler;
 }
 
-// Falls das Spiel mit unentschieden endet, zeige eine dazugehörige Animation
+// Falls das Spiel unentschieden endet, zeige eine dazugehörige Animation
 void SpielUnentschieden( void )
 {
   bool neuesSpiel = false;
 
-  // Zuerst bleibt die Anzeig für 2 Sekunden auf dem aktuellen Spielstand
+  // Zuerst bleibt die Anzeige für 2 Sekunden auf dem aktuellen Spielstand
   unsigned long Counter = millis();
   while((millis() - 2000) < Counter)
   {
     AusgabeLEDs();
-  }
-
-  // Danach wird noch zwischengespeichert, welcher Spieler als nächstes an der Reihe wäre
-  // Das ist notwendig, weil später dieser Wert verloren geht
-  int naechsterSpieler = Gewinner + 1;
-  if (naechsterSpieler > 2)
-  {
-    naechsterSpieler = 1;
   }
 
   // Solange keine Taste gedrückt wird, wird nun diese Animation angezeigt
@@ -433,16 +442,17 @@ void SpielUnentschieden( void )
   }while(!neuesSpiel);
   
   InitVariablen();
-  Spieler = naechsterSpieler;
 }
 
 // Die Setup-Funktion wird beim ersten Start aufgerufen
 void setup() {
-  Init();
+  InitIO();
   AllesAus();
   InitVariablen();
-
-  //Serial.begin(9600);
+  if (DEBUG)
+  {
+    Serial.begin(9600);
+  }
 }
 
 // Die Loop Funktion wird endlos durchlaufen
@@ -450,19 +460,37 @@ void loop() {
 
   // ermittle, ob eine Tastee geddrückt wurde
   int Taste = TasteGedrueckt();
+  int Gewinner;
 
   // Falls eine Taste gedrückt wurde und dieses Feld noch unbelegt ist, markiere es mit dem aktuellen Spieler
   if ((Taste != 10) && (Spielfeld[ Taste ] == 0))
   {
-    Spielfeld[ Taste ] = Spieler;
-    Spieler++;
+    Spielfeld[ Taste ] = AktiverSpieler;
+
+    // Nach jedem gültigen Spielzug ist der neue Spieler an der Reihe. Dafür wird der Spieler zuerst inkrementiert und danach ein Überlauf geprüft
+    AktiverSpieler++;
+    if (AktiverSpieler > 2)
+    {
+      AktiverSpieler = 1;
+    }
+
+    if (DEBUG)
+    {
+      Serial.println("Spielfeld:");
+      for(int feld = 0; feld < 9; feld++)
+      {
+        if (feld%3 == 2) {
+          // Wir sind am Ende einer Zeile
+          Serial.println(Spielfeld[feld]);
+        } else {
+          Serial.print(Spielfeld[feld]);
+        }
+      }
+      Serial.print("Aktiver Spieler: ");
+      Serial.println(AktiverSpieler);
+    }
   }
 
-  // Nach jedem Spielzug ist der neue Spieler an der Reihe. Dafür wird der Spieler zuerst inkrementiert und danach ein Übrelauf geprüft
-  if (Spieler > 2)
-  {
-    Spieler = 1;
-  }
 
   // An der Status-LED wird gezeigt, welcher Spieler gerade an der Reihe ist
   AusgabeStatusLED();
@@ -471,33 +499,21 @@ void loop() {
   AusgabeLEDs();
 
   // Es wird geprüft, ob es schon einen Gewinner gibt
-  PruefeGewinner();
+  Gewinner = PruefeGewinner();
 
-  //Serial.println(Gewinner);
+  if (DEBUG && Gewinner)
+  {
+    Serial.print("Status Gewinner: ");
+    Serial.println(Gewinner);
+  }
 
-  // Falle es einen Gewinner gibt, wird die dazugehörige Animation aktiviert
+  // Aktiviere passende Animation, falls das Spiel durch Sieg oder Unentschieden beendet wurde
   if(Gewinner > 0)
   {
-    SpielGewonnen();
+    SpielGewonnen(Gewinner);
   }
-  // Falls das Spiel unentscheiden steht, wird die dazugrhörige Animation aktiviert
-  if(Gewinner == -1)
+  else if(Gewinner == -1)
   {
     SpielUnentschieden();
   }
-  
-  /*
-  for(int i = 0; i < 9; i++)
-  {
-    Serial.print(Spielfeld[i]);
-    Serial.print("; ");
-  }
-  Serial.print("   ");
-  Serial.print(analogRead(A7));
-  Serial.print("   ");
-  Serial.print(Taste);
-  Serial.print("   ");
-  Serial.print(Spieler);
-  Serial.println(" ");
-  */
 }
